fa4f867036a5bf8b4593bf66d02d03848305c8d3
KwameWilson <kwamidzorgbenyuie@gmail.com>
2024-04-09 15:43:44 +0000

    Making the page dynamic

--- dictionary-web-app/public/main.js
+++ dictionary-web-app/public/main.js
@@ -9,19 +9,65 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const word = document.getElementById('search');
+const displaySearchWord = document.getElementById('display');
+const phonetics = document.getElementsByClassName('phonetics');
 const search = document.querySelector('.search-bar img');
 const toggle = document.querySelector('#theme-toggle');
-search.addEventListener('click', () => {
-    console.log('Has been clicked');
-    function fetchData() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`)
-                .then(resp => {
-                // console.log(resp.json());
-                return resp.json();
-            }).catch(error => console.error('Error!', error));
-            console.log(response);
-        });
-    }
-    fetchData();
-});
+const wordArea = document.querySelector('.word-area');
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`)
+            .then(resp => {
+            // console.log(resp.json());
+            return resp.json();
+        })
+            .then(data => {
+            wordArea.innerHTML = `
+                
+                <div class="search-term">
+                <div class="word">
+                    <h1 id="display">${data[0].word}</h1>
+                    <div class="phonetics">
+                        ${data[0].phonetic}
+                    </div>
+                </div>
+
+                <div class="play-button">
+                    <img src="img/icon-play.svg" alt="play-button">
+                </div>
+            </div>
+
+            <div class="search-info">
+                <div>
+                    <h3>${data[0].meanings[0].partOfSpeech}</h3>
+                    <span></span>
+                </div>
+                
+
+                <div class="definition">
+                    <p>Meaning</p>
+                    <ul>
+                        <li><span>${data[0].meanings[0].definitions[0].definition}, ${data[0].meanings[0].definitions[1].definition}</span></li>
+                    </ul>
+
+                    <p>Synonyms <span>electronic keyboard</span></p> 
+                </div>
+
+                <div class="alt-definition">
+                    <div>
+                        <h3>${data[0].meanings[1].partOfSpeech}</h3>
+                    <span></span>
+                    </div>
+                    
+                    <p>Meaning</p>
+                    <ul>
+                        <li><span>${data[0].meanings[1].definitions[0].definition}</span></li>
+                        <p>“Keyboarding is the part of this job I hate the most.”</p>
+                    </ul>
+                </div>
+            </div>
+        </div> `;
+        }).catch(error => console.error('Error!', error));
+    });
+}
+search.addEventListener('click', fetchData);
--- dictionary-web-app/src/main.ts
+++ dictionary-web-app/src/main.ts
@@ -1,22 +1,69 @@
 const word: HTMLInputElement = document.getElementById('search');
+const displaySearchWord = document.getElementById('display');
+const phonetics = document.getElementsByClassName('phonetics')
 const search: HTMLImageElement = document.querySelector('.search-bar img');
 const toggle: HTMLInputElement = document.querySelector('#theme-toggle')
 
+const wordArea = document.querySelector('.word-area')
 
-search.addEventListener('click', () => {
+async function fetchData() {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`)
 
-    console.log('Has been clicked');
-    async function fetchData() {
-        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`)
-            .then(resp => {
-                // console.log(resp.json());
+        .then(resp => {
+            // console.log(resp.json());
+            return resp.json();
+        })
+        .then(data => {
+            wordArea.innerHTML = `
+                
+                <div class="search-term">
+                <div class="word">
+                    <h1 id="display">${data[0].word}</h1>
+                    <div class="phonetics">
+                        ${data[0].phonetic}
+                    </div>
+                </div>
 
-                return resp.json();
-            }).catch(error => console.error('Error!', error))
+                <div class="play-button">
+                    <img src="img/icon-play.svg" alt="play-button">
+                </div>
+            </div>
 
-        console.log(response);
-    }
-    fetchData()
+            <div class="search-info">
+                <div>
+                    <h3>${data[0].meanings[0].partOfSpeech}</h3>
+                    <span></span>
+                </div>
+                
 
-})
+                <div class="definition">
+                    <p>Meaning</p>
+                    <ul>
+                        <li><span>${data[0].meanings[0].definitions[0].definition}, ${data[0].meanings[0].definitions[1].definition}</span></li>
+                    </ul>
+
+                    <p>Synonyms <span>electronic keyboard</span></p> 
+                </div>
+
+                <div class="alt-definition">
+                    <div>
+                        <h3>${data[0].meanings[1].partOfSpeech}</h3>
+                    <span></span>
+                    </div>
+                    
+                    <p>Meaning</p>
+                    <ul>
+                        <li><span>${data[0].meanings[1].definitions[0].definition}</span></li>
+                        <p>“Keyboarding is the part of this job I hate the most.”</p>
+                    </ul>
+                </div>
+            </div>
+        </div> `
+
+
+
+        }).catch(error => console.error('Error!', error))
+}
+
+search.addEventListener('click', fetchData)
 
